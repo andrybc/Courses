4cc1476a8c659da0c6a01587bcc6e150c17f74e8
andrybc <canelbandry@gmail.com>
2023-08-24 11:13:36 -0400

    first try of problem. Added a function to provide frequency arrays for both sign names...and then run a single forloop to check how many letters needed to be added

---
+++ COP3502/signoftrouble.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_STRING_SIZE 100002
+
+void calculateFreq(const char* signName, int* frequencyArray){
+
+        for (size_t i = 0; signName[i] != '\0'; i++) {
+                
+            frequencyArray[signName[i] - 'A']++;
+
+        }
+
+}
+
+int main(){
+    //declaring both strings as char arrays
+
+    char *oldSignName;
+    char *newSignName;
+    int frequencyOldArray[26] = {0}; // initialize all elements to 0
+    int frequencyNewArray[26] = {0}; 
+    int count = 0;
+
+    oldSignName = (char *)calloc(MAX_STRING_SIZE , sizeof(char));
+    newSignName = (char *)calloc(MAX_STRING_SIZE , sizeof(char));
+
+    fgets(oldSignName, MAX_STRING_SIZE,stdin);
+    fgets(newSignName, MAX_STRING_SIZE,stdin);
+
+    calculateFreq(oldSignName, frequencyOldArray);
+    calculateFreq(newSignName, frequencyNewArray);
+
+    for(int i = 0; i <26; i++){
+
+        if(frequencyNewArray > frequencyOldArray){
+            count += (frequencyNewArray-frequencyOldArray);
+        }
+
+    }
+
+    printf("The amount of new letters needed is: %d", count);
+
+
+    free(oldSignName);
+    free(newSignName);
+    return 0;
+}
